a3c08774a4c799982628a1880fee11d1a1da72b5
gwind55 <gwind55@hotmail.om>
2020-11-07 00:09:57 +0800

    update 6.0.20

--- src/Library.php
+++ src/Library.php
@@ -19,10 +19,10 @@ use think\admin\command\Database;
 use think\admin\command\Install;
 use think\admin\command\Queue;
 use think\admin\command\Version;
-use think\admin\multiple\App;
 use think\admin\multiple\command\Build;
 use think\admin\multiple\command\Clear;
-use think\admin\multiple\Url;
+use think\admin\multiple\Multiple;
+use think\admin\multiple\BuildUrl;
 use think\admin\service\AdminService;
 use think\admin\service\SystemService;
 use think\middleware\LoadLangPack;
@@ -39,9 +39,9 @@ use function Composer\Autoload\includeFile;
 class Library extends Service
 {
     /**
-     * 扩展库版本号
+     * 版本号
      */
-    const VERSION = '6.0.17';
+    const VERSION = '6.0.20';
 
     /**
      * 启动服务
@@ -50,14 +50,14 @@ class Library extends Service
     {
         // 多应用中间键处理
         $this->app->event->listen('HttpRun', function (Request $request) {
-            $this->app->middleware->add(App::class);
+            $this->app->middleware->add(Multiple::class);
             // 解决 HTTP 调用 Console 之后 URL 问题
             if (!$this->app->request->isCli()) {
                 $request->setHost($request->host());
             }
         });
         // 替换 ThinkPHP 地址
-        $this->app->bind('think\route\Url', Url::class);
+        $this->app->bind('think\route\Url', BuildUrl::class);
         // 替换 ThinkPHP 指令
         $this->commands(['build' => Build::class, 'clear' => Clear::class]);
         // 注册 ThinkAdmin 指令
@@ -82,22 +82,23 @@ class Library extends Service
                 $this->app->request->setPathinfo($_SERVER['argv'][1]);
             }
         } else {
-            $isSess = $this->app->request->request('not_init_session', 0) == 0;
-            $notYar = stripos($this->app->request->header('user-agent', ''), 'PHP Yar RPC-') === false;
-            if ($notYar && $isSess) {
+            $issess = intval($this->app->request->get('not_init_session', '0')) === 0;
+            $notapi = stripos($this->app->request->header('user_agent', ''), 'PHP Yar RPC-') === false;
+            if ($notapi && $issess) {
                 // 注册会话初始化中间键
                 $this->app->middleware->add(SessionInit::class);
                 // 注册语言包处理中间键
                 $this->app->middleware->add(LoadLangPack::class);
             }
             // 注册访问处理中间键
-            $this->app->middleware->add(function (Request $request, \Closure $next) {
+            $this->app->middleware->add(function (Request $request, \Closure $next) use ($issess, $notapi) {
                 $header = [];
                 if (($origin = $request->header('origin', '*')) !== '*') {
                     $header['Access-Control-Allow-Origin'] = $origin;
-                    $header['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE';
-                    $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With';
+                    $header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,PATCH,DELETE';
+                    $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With,User-Form-Token,User-Token,Token';
                     $header['Access-Control-Expose-Headers'] = 'User-Form-Token,User-Token,Token';
+                    $header['Access-Control-Allow-Credentials'] = 'true';
                 }
                 // 访问模式及访问权限检查
                 if ($request->isOptions()) {
@@ -105,14 +106,19 @@ class Library extends Service
                 } elseif (AdminService::instance()->check()) {
                     return $next($request)->header($header);
                 } elseif (AdminService::instance()->isLogin()) {
-                    return json(['code' => 0, 'msg' => lang('think_library_not_auth')])->header($header);
+                    return json(['code' => 0, 'info' => lang('think_library_not_auth')])->header($header);
                 } else {
-                    return json(['code' => 0, 'msg' => lang('think_library_not_login'), 'url' => sysuri('admin/login/index')])->header($header);
+                    return json(['code' => 0, 'info' => lang('think_library_not_login'), 'url' => sysuri('admin/login/index')])->header($header);
                 }
             }, 'route');
         }
-        // 动态加入应用函数
-        $SysRule = "{$this->app->getBasePath()}*/sys.php";
-        foreach (glob($SysRule) as $file) includeFile($file);
+        // 动态加载应用初始化系统函数
+        [$ds, $base] = [DIRECTORY_SEPARATOR, $this->app->getBasePath()];
+        foreach (glob("{$base}*{$ds}sys.php") as $file) includeFile($file);
+        // 动态加载插件初始化系统函数
+        $base = "{$this->app->getBasePath()}addons{$ds}";
+        if (file_exists($base) && is_dir($base)) {
+            foreach (glob("{$base}*{$ds}sys.php") as $file) includeFile($file);
+        }
     }
 }
